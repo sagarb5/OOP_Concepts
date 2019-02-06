e5c039e292ecc57e89f46267690a890d9493f62b
sagar26197@gmail.com <sagar26197@gmail.com>
2019-02-05 20:04:08 -0500

    v2

---
+++ OOP_Concepts/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Concepts
+{
+    class Person
+    {
+        public string firstName; 
+        public string lastName;
+        public string salutation;
+
+        public string getName()
+        {
+            return salutation + "  " + firstName + " " + lastName;
+        }
+    }
+}
--- OOP_Concepts/Program.cs
+++ OOP_Concepts/Program.cs
@@ -6,7 +6,10 @@ namespace OOP_Concepts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            Person Student = new Person { salutation = "Well Congrats!", firstName = "Sagar", lastName = "Billore"};
+            Console.WriteLine(Student.getName());
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); // Exit Statement
+        } // end of main
+    }// end of class program
 }
